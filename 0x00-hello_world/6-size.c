253f70ca670543bd488906975b7b3a32657e8b37
Mercileen <kamwindamercy@gmail.com>
2023-06-17 08:38:09 -0700

    intel syntax

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,5 +7,9 @@
  */
 int main(void)
 {
-    return (0);
+	printf("Size of char:%d bytes(s)\n",sizeof(char));
+	printf("Size of an int:%d bytes(s),sizeof (int));
+	printf("Size of a long int:%d bytes(s)\n",Sizeoflong(long int))
+	printf("Size of a float:%d bytes(s)\n",sizeof(float));
+	return (0);
 }
