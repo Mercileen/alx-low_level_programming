a079f87e0e6795a0991d486224b87da61817e592
Mercileen <kamwindamercy@gmail.com>
2023-06-17 05:32:59 -0700

    print with put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,4 +7,6 @@ include <stdio.h>
  */
 int main(void)
 {
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+     	return (0);
+}
